28dae4a87ed54f59b49e99ffa4f2605737475e93
Miroslaw-Keil <Miroslaw.Keil@Student.HTW-Berlin.de>
2022-10-12 14:23:49 +0200

    Controller and Template

--- src/main/java/test/demo/GreetingController.java
+++ src/main/java/test/demo/GreetingController.java
@@ -1,4 +1,4 @@
-package com.example.servingwebcontent;
+package test.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/test/demo/ServingWebContentApplication.java
+++ src/main/java/test/demo/ServingWebContentApplication.java
@@ -1,4 +1,4 @@
-package com.example.servingwebcontent;
+package test.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
